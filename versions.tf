a115b87de512f58d685c92c5aa06415f0e52dd4f
Charles Bushong <cbushong@fearless.tech>
2020-08-28 14:39:49 -0400

    Bump aws provider version

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 2"
+      version = ">= 2.0, < 4.0.0"
     }
     archive = {
       source  = "hashicorp/archive"
