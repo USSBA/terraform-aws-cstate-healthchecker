df59f285c99e6bd54f39af5635084689f5c94d13
Jason Smigovsky <jsmigovsky@fearless.tech>
2020-11-03 14:21:16 -0500

    IA-1654: downtime threadhold calc was introduced

--- code/index.py
+++ code/index.py
@@ -13,8 +13,11 @@ ssm = boto3.client('ssm')
 # Current Time
 def current_time():
   return datetime.utcnow().replace(tzinfo=timezone.utc).isoformat(timespec='seconds')
+
+# Current Time Filename
 def current_time_filename():
   return datetime.utcnow().strftime('%Y%m%d-%H%M%S')
+
 # Fetch Template
 def fetch_template(template_file_path, repo, branch = "master"):
   template_contents = None
@@ -27,7 +30,13 @@ def fetch_template(template_file_path, repo, branch = "master"):
     print("WARN: Template not found")
   return template_contents
 
+# Message Age
+def age_in_minutes(contents):
+  now = datetime.utcnow().replace(tzinfo=timezone.utc)
+  entry_date = datetime.fromisoformat(re.search(r'^(date:) (.*)', contents, flags=re.MULTILINE).group(2))
+  return int((now - entry_date).total_seconds() / 60)
 
+# Lambda (Main)
 def lambda_handler(event, context):
   sns_message = json.loads(event['Records'][0]['Sns']['Message'])
   current_state = sns_message['NewStateValue']
@@ -56,24 +65,33 @@ def lambda_handler(event, context):
     # Found Existing `latest` File, implying active incident; updating if necessary
     old_file_contents = base64.b64decode(old_file.content).decode("utf-8")
     if current_state == "OK":
-      # Status is okay, updating file to archive existing issue
-      resolved_file_contents=re.sub(r'^(resolved:) .*', r'\1 true', old_file_contents, flags=re.MULTILINE)
-      # Set resolvedWhen to current time
-      resolved_file_contents=re.sub(r'^(resolvedWhen:) .*', rf'\1 {current_time()}', resolved_file_contents, flags=re.MULTILINE)
-      resolved_file_path=re.sub("latest", current_time_filename(), latest_file)
-      try:
-        repo.create_file(resolved_file_path, message=f"Automatic update for {application_name}: resolved incident", content=resolved_file_contents, branch=branch)
-        print(f"INFO: Latest file moved to {resolved_file_path}")
+      # If the message was created within the last 5 minutes is will be discarded
+      if message_age(old_file_contents) <= 5:
         try:
           repo.delete_file(latest_file, message=f"Automatic update for {application_name}: delete latest_file", branch=branch, sha=old_file.sha)
+          print(f"INFO: Latest file removed, event within acceptable timeframe")
         except Exception as err:
           print(f"ERROR: Could not delete old latest file: path={latest_file}")
           print(err)
-      except Exception as err:
-        print(f"ERROR: Could not create new resolved_file: path={resolved_file_path}")
-        print(err)
-    else:
-      print(f"INFO: Latest file exists, current_state={current_state}; no updates necessary")
+      else:
+        # Status is okay, updating file to archive existing issue
+        resolved_file_contents=re.sub(r'^(resolved:) .*', r'\1 true', old_file_contents, flags=re.MULTILINE)
+        # Set resolvedWhen to current time
+        resolved_file_contents=re.sub(r'^(resolvedWhen:) .*', rf'\1 {current_time()}', resolved_file_contents, flags=re.MULTILINE)
+        resolved_file_path=re.sub("latest", current_time_filename(), latest_file)
+        try:
+          repo.create_file(resolved_file_path, message=f"Automatic update for {application_name}: resolved incident", content=resolved_file_contents, branch=branch)
+          print(f"INFO: Latest file moved to {resolved_file_path}")
+          try:
+            repo.delete_file(latest_file, message=f"Automatic update for {application_name}: delete latest_file", branch=branch, sha=old_file.sha)
+          except Exception as err:
+            print(f"ERROR: Could not delete old latest file: path={latest_file}")
+            print(err)
+        except Exception as err:
+          print(f"ERROR: Could not create new resolved_file: path={resolved_file_path}")
+          print(err)
+      else:
+        print(f"INFO: Latest file exists, current_state={current_state}; no updates necessary")
   except:
     # File not found, create instead of update
     if current_state == "ALARM":
